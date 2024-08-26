b03255bcaeedf95feb7d44ea7b06c81dbbe87bc6
shreyan241 <shreyansood@gmail.com>
2024-08-26 11:46:10 -0700

    added resume, typewriter effect and portrait image

--- assets/js/main.js
+++ assets/js/main.js
@@ -160,3 +160,33 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+const typewriter = document.querySelector('.typewriter');
+const words = ["a Data Science Grad", "a Machine Learning Researcher", "a Computer Vision Developer", "a Statistical Modeler", "a Data Storyteller"];
+let i = 0;
+let j = 0;
+let currentWord = '';
+let isDeleting = false;
+
+function type() {
+    if (isDeleting) {
+        currentWord = words[i].substring(0, j--);
+    } else {
+        currentWord = words[i].substring(0, j++);
+    }
+    
+    typewriter.textContent = currentWord;
+
+    if (!isDeleting && j === words[i].length) {
+        setTimeout(() => isDeleting = true, 1000);
+    } else if (isDeleting && j === 0) {
+        i = (i + 1) % words.length;
+        isDeleting = false;
+    }
+
+    setTimeout(type, isDeleting ? 50 : 150);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(type, 500);
+});
